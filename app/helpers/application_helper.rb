382962c9f4a4fdd55fe684be3686d724626fed8a
Jacob Pedersen <jaacib@gmail.com>
2009-10-16 22:46:19 +0200

    Views for login and signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def page_title
+    @page_title || action_name.to_s.humanize
+  end
+  
+  def tabular_form_for(name, object = nil, options = nil, &proc)
+    concat("<table class=\"tabular_form\">", @_binding)
+    form_for(name, object, (options||{}).merge(:builder => TabularFormBuilder), &proc)
+    concat("</table>", @_binding)
+  end
+  
 end
